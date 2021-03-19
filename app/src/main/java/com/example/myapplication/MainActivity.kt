960cd902e3e6219c52d22505cf9cc1de9ab01e22
jtue <jtue@vols.utk.edu>
2021-03-19 18:22:52 -0400

    Changed tabs within the app to read 'QR Scanner, Account, and Orders', as well as added the third tab itself. All three still require a layout (that differs from the other two).

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -23,7 +23,7 @@ class MainActivity : AppCompatActivity() {
         val fab: FloatingActionButton = findViewById(R.id.fab)
 
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+            Snackbar.make(view, "Will be removed/replaced with Scanner Action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
     }
