0c9a0a859f9c0e6b5b5112cc7786e2b977bb6b9e
Tristen-Finley <59655247+Tristen-Finley@users.noreply.github.com>
2021-03-25 00:17:49 -0500

    Button Works, But Scanned Message isnt Showing Up

--- app/src/main/java/com/example/myapplication/ui/main/ScannerFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/main/ScannerFragment.kt
@@ -4,6 +4,7 @@ import android.R.attr
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -14,7 +15,6 @@ import com.example.myapplication.R
 import com.google.zxing.integration.android.IntentIntegrator
 import androidx.fragment.app.Fragment as Fragment1
 
-
 class ScannerFragment : Fragment1() {
 
     override fun onCreateView(
@@ -24,24 +24,34 @@ class ScannerFragment : Fragment1() {
         val view: View = inflater.inflate(R.layout.fragment_scanner, container, false)
         val btn: Button = view.findViewById(R.id.scan_button)
 
-        btn.setOnClickListener{
 
-            IntentIntegrator(activity).initiateScan(); // `this` is the current Fragment
+        btn.setOnClickListener {
+
+            val scanner = IntentIntegrator(activity)
+            scanner.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE)
+            scanner.setBeepEnabled(false)
+            scanner.initiateScan()
         }
 
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_scanner, container, false)
+        return view
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
+        Log.i("dwdf", "deqwdfwefdew")
         if(resultCode == Activity.RESULT_OK){
             val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+            Log.i("dwdf", "deqwdfwefdew")
             if (result != null) {
                 if (result.contents == null) {
-                    Toast.makeText(context, "Cancelled", Toast.LENGTH_LONG).show()
+                    Log.e("scan error!", "Cancelled")
+                    System.out.printf("Cancelled")
+                    Toast.makeText(context, "Cancelled", Toast.LENGTH_SHORT).show()
                 } else {
-                    Toast.makeText(context, "Scanned: " + result.contents, Toast.LENGTH_LONG).show()
+                    Log.i("Scanned", result.contents)
+                    System.out.printf("Scanned" + result.contents)
+                    Toast.makeText(context, "Scanned: " + result.contents, Toast.LENGTH_SHORT).show()
                 }
             } else {
                 super.onActivityResult(requestCode, resultCode, data)
