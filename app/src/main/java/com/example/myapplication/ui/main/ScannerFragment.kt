34231ba0cf3c22782ef8c97c141dd0ac08f3c2f1
Tristen-Finley <59655247+Tristen-Finley@users.noreply.github.com>
2021-04-06 18:34:53 -0500

    Changed scanner library, it now works.

--- app/src/main/java/com/example/myapplication/ui/main/ScannerFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/main/ScannerFragment.kt
@@ -11,51 +11,42 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.Toast
 import androidx.fragment.app.FragmentManager
+import com.budiyev.android.codescanner.CodeScanner
+import com.budiyev.android.codescanner.CodeScannerView
+import com.budiyev.android.codescanner.DecodeCallback
 import com.example.myapplication.R
-import com.google.zxing.integration.android.IntentIntegrator
 import androidx.fragment.app.Fragment as Fragment1
 
 class ScannerFragment : Fragment1() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val view: View = inflater.inflate(R.layout.fragment_scanner, container, false)
-        val btn: Button = view.findViewById(R.id.scan_button)
+    private lateinit var codeScanner: CodeScanner
 
-
-        btn.setOnClickListener {
-
-            val scanner = IntentIntegrator(activity)
-            scanner.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE)
-            scanner.setBeepEnabled(false)
-            scanner.initiateScan()
-        }
-
-        // Inflate the layout for this fragment
-        return view
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_scanner, container, false)
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        Log.i("dwdf", "deqwdfwefdew")
-        if(resultCode == Activity.RESULT_OK){
-            val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
-            Log.i("dwdf", "deqwdfwefdew")
-            if (result != null) {
-                if (result.contents == null) {
-                    Log.e("scan error!", "Cancelled")
-                    System.out.printf("Cancelled")
-                    Toast.makeText(context, "Cancelled", Toast.LENGTH_SHORT).show()
-                } else {
-                    Log.i("Scanned", result.contents)
-                    System.out.printf("Scanned" + result.contents)
-                    Toast.makeText(context, "Scanned: " + result.contents, Toast.LENGTH_SHORT).show()
-                }
-            } else {
-                super.onActivityResult(requestCode, resultCode, data)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val scannerView = view.findViewById<CodeScannerView>(R.id.scanner_view)
+        val activity = requireActivity()
+        codeScanner = CodeScanner(activity, scannerView)
+        codeScanner.decodeCallback = DecodeCallback {
+            activity.runOnUiThread {
+                Toast.makeText(activity, it.text, Toast.LENGTH_LONG).show()
             }
         }
+        scannerView.setOnClickListener {
+            codeScanner.startPreview()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        codeScanner.startPreview()
+    }
+
+    override fun onPause() {
+        codeScanner.releaseResources()
+        super.onPause()
     }
 }
