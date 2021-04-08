0ba666e6ce0c525213353be73fae7637665d9383
Tristen-Finley <59655247+Tristen-Finley@users.noreply.github.com>
2021-04-07 19:42:03 -0500

    added google services json to github tracking

--- app/src/main/java/com/example/myapplication/ui/main/SettingsFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/main/SettingsFragment.kt
@@ -12,7 +12,6 @@ class SettingsFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 
     override fun onCreateView(
