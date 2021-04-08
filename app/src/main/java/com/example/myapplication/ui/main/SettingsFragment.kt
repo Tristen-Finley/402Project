2fd02f3a886ccc81f9ba5e143bec9e833f26143b
Tristen-Finley <59655247+Tristen-Finley@users.noreply.github.com>
2021-04-07 21:58:39 -0500

    settings now properly display

--- app/src/main/java/com/example/myapplication/ui/main/SettingsFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/main/SettingsFragment.kt
@@ -1,41 +1,16 @@
 package com.example.myapplication.ui.main
 
-import android.content.SharedPreferences
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.preference.PreferenceManager
+import android.widget.Toast
+import androidx.preference.PreferenceFragmentCompat
 import com.example.myapplication.R
 
+class SettingsFragment : PreferenceFragmentCompat() {
 
-class SettingsFragment : Fragment() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.root_preferences, rootKey)
 
-    override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+        Toast.makeText(context, "These are your settings", Toast.LENGTH_SHORT).show()
     }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        loadSettings()
-    }
-
-
-    private fun loadSettings() {
-        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
-
-        val username = sharedPreferences.getString("username", "")
-        val email = sharedPreferences.getString("email", "")
-        //val password = sharedPreferences.getString("password", "")
-
-    }
-
 }
