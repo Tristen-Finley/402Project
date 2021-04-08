54b6c4ccedd44918cc42c7fc002e33be78c99b14
jtue <jtue@vols.utk.edu>
2021-04-07 20:55:34 -0400

    created starter Preferences page in root_preferences.xml, needs to be displayed on the SettingsFragment.kt and/or fragment_settings.xml.

--- app/src/main/java/com/example/myapplication/ui/main/SettingsFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/main/SettingsFragment.kt
@@ -1,29 +1,41 @@
 package com.example.myapplication.ui.main
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.preference.PreferenceManager
 import com.example.myapplication.R
 
 
 class SettingsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_settings, container, false)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        loadSettings()
+    }
+
+
+    private fun loadSettings() {
+        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
+
+        val username = sharedPreferences.getString("username", "")
+        val email = sharedPreferences.getString("email", "")
+        //val password = sharedPreferences.getString("password", "")
+
     }
 
 }
