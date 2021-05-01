bdf7af0f2282f1e84ec502c85e13d14520713f79
Tristen-Finley <59655247+Tristen-Finley@users.noreply.github.com>
2021-05-01 18:02:51 -0500

    Merge issues are now resolved, ready to be merged top master

--- app/src/main/java/com/example/myapplication/ui/main/SettingsFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/main/SettingsFragment.kt
@@ -4,22 +4,25 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.preference.PreferenceFragmentCompat
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import com.example.myapplication.R
 
 class SettingsFragment : PreferenceFragmentCompat() {
-
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
 
         Toast.makeText(context, "These are your settings", Toast.LENGTH_SHORT).show()
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+        fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+        ): View? {
+            // Inflate the layout for this fragment
+            return inflater.inflate(R.layout.fragment_settings, container, false)
 
+        }
     }
+
 }
